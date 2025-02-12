7306d5aaa4d8673996cd4ad3dcd1e89cd74e2c89
ayoshin82 <ayoshin82@.gmail.com>
2025-02-12 18:47:46 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i =i + 1) {
+            System.out.println(i);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
